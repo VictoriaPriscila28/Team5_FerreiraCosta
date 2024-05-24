6282ab50755c087992537c007254833cbcb2216a
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-05-24 09:25:19 -0300

    add cliente configuration

--- GerenciamentoDeBiblioteca.Infra.Data/EntitiesConfiguration/ClienteConfiguration.cs
+++ GerenciamentoDeBiblioteca.Infra.Data/EntitiesConfiguration/ClienteConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GerenciamentoDeBiblioteca.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,19 @@ using System.Threading.Tasks;
 
 namespace GerenciamentoDeBiblioteca.Infra.Data.EntitiesConfiguration
 {
-    internal class ClienteConfiguration
+    internal class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
     {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.CliCPF).HasMaxLength(11).IsRequired();
+            builder.Property(x => x.CliNome).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.CliEndereco).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.CliCidade).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.CliBairro).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.CliNumero).HasMaxLength(20).IsRequired();
+            builder.Property(x => x.CliTelefoneCelular).HasMaxLength(11).IsRequired();
+            builder.Property(x => x.CliTelefoneFixo).HasMaxLength(10).IsRequired();
+        }
     }
 }
